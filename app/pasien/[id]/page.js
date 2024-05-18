9c0961e809f2358c43b9c04a471204a8f64e2b46
MUHAMMAD FADHEL H <fadhelhayat17@gmail.com>
2024-05-18 18:15:06 +0800

    menggunakan onValue untuk update data pada detail pasien

--- app/pasien/[id]/page.js
+++ app/pasien/[id]/page.js
@@ -1,31 +1,63 @@
 'use client'
 
 import { useEffect, useState } from "react";
-import { useParams, useRouter } from "next/navigation";
-import { get, ref } from 'firebase/database';
+import { useParams } from "next/navigation";
+import { get, onValue, ref } from 'firebase/database';
 import { database } from "../../firebaseConfig";
+import { Line } from "react-chartjs-2";
+import { 
+    Chart as Chartjs, 
+    CategoryScale,
+    LinearScale,
+    PointElement, 
+    LineElement, 
+    Title, 
+    Tooltip, 
+    Legend 
+} from "chart.js";
+
+Chartjs.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 export default function PatientDetail() {
     const { id } = useParams();
     const [patient, setPatient] = useState(null);
+    const [infusionData, setInfusionData] = useState([]);
 
     useEffect(() => {
         if (id) {
             const patientRef = ref(database, `pasien/${id}`);
-            get(patientRef).then((snapshot) => {
+
+            const unsubscribe = onValue(patientRef, (snapshot) => {
                 if (snapshot.exists()) {
                     setPatient(snapshot.val());
                 } else {
                     console.error('Pasien tidak ditemukan');
                 }
-            }).catch(error => {
-                console.error("error retrieving patient:", error);
+            }, error => {
+                console.error("error:", error);
             });
+
+            return () => unsubscribe();
+            
         }
     }, [id]);
 
     if (!patient) {
         return <div>LOADING...</div>
+    }
+
+    const labels = infusionData.map(entry => entry.time);
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: 'Kecepatan Infus (TPM)',
+                data: infusionData.map(entry => entry.drip_rate),
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1
+            }
+        ]
     }
 
     return (
@@ -35,6 +67,10 @@ export default function PatientDetail() {
             <p>Nomor Kamar: {patient.nomor_kamar}</p>
             <p>kecepatan infus (TPM): {patient.drip_rate.toFixed(2)}</p>
             <p>Cairan infus: {patient.infusion_weight}</p>
+
+            <div className="chart-container">
+                <Line data={data}/>
+            </div>
         </div>
     )
 }
